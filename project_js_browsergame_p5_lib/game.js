89e5c73438d0b96a18b8a95f2e6662369628d8c6
Darius Silski <me@here.com>
2018-09-27 14:56:22 +0200

    initial commit for browsergame with p5.js

---
+++ project_js_browsergame_p5_lib/game.js
@@ -0,0 +1,42 @@
+// Ball
+var ctBall
+// Leinwand
+var canvasWidth= 1080 / 3;
+var canvasHeight = 1920 / 3;
+var ballSize = canvasHeight / 15;
+
+function setup() {
+  createCanvas(canvasWidth, canvasHeight);
+  noStroke();
+  ctBall = new CtBall();
+}
+
+function draw() {
+  background(0,136,255);
+  ctBall.show();
+}
+
+function CtBall() {
+  this.x = canvasWidth / 2;
+  this.y = canvasHeight / 2;
+  this.speed = 0;
+  this.acceleration = 1;
+
+  this.show = function () {
+    this.speed += this.acceleration;
+    // Veränderung Position
+    this.y += this.speed;
+    // Ball kommt auf den Boden auf
+    if(this.y > (canvasHeight - ballSize/2) && this.speed > 0) {
+      this.jump();
+    }
+    // Ball wird gezeichnet
+    fill(252,233,3);
+    ellipse(this.x, this.y, ballSize, ballSize);
+  }
+  //Ball prallt ab:
+  this.jump = function() {
+    this.speed = this.speed * -0.9;
+    console.log(this.speed);
+  }
+}
